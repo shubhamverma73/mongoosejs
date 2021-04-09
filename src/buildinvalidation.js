017e8aaf5af93e49575140a72c8913915f5b8ad5
Shubham <shubhamkrverma73@gmail.com>
2021-04-09 18:08:38 +0530

    build in validation

---
+++ src/buildinvalidation.js
@@ -0,0 +1,44 @@
+//https://mongoosejs.com/docs/schematypes.html
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/reactJS', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('connected');
+}).catch(err => {
+    console.log('Error'+err);
+});
+
+// Schema
+const playlistSchema = new mongoose.Schema({
+    name:   { type: String, required: true, unique: true, uppercase: true, minlength: [2, 'Minimum 2 characters are allowed'], maxlength: 15 },
+    ctype:  String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: { type: Date, default: Date.now }
+});
+
+// Collection create
+const Playlist = new mongoose.model('Playlist', playlistSchema);
+
+// Create document or insert data
+const createDocument = async () => {
+    try {
+        const reactPlaylist = new Playlist({
+            name:   'S',
+            ctype:  'Back END',
+            videos: 420,
+            author: 'Shubham',
+            active: true,
+        });        
+        const result = await reactPlaylist.save();
+        console.log(result);
+    }
+    catch(err) {
+        console.log(err);
+    }
+    
+}
+createDocument();
